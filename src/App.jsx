8aa4a58f724fadc10eaf0c9ccc8effca75f6ef1d
anhelina29 <linakh358@gmail.com>
2025-09-16 14:33:49 +0300

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {RegistrationForm} from "./components/Form.jsx";
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <RegistrationForm />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,56 @@
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import {object, string} from 'yup';
+
+export const FormValid = () => object().shape({
+    name: string()
+        .min(2, 'Min length must be greater than 2 characters')
+        .max(50, 'Max length must not be greater than 50 characters')
+        .required('Please type your name'),
+
+    email: string()
+        .email('Please enter a valid email address')
+        .required('Please type your email address'),
+
+    password: string()
+        .min(8, 'Password must be at least 8 characters')
+        .max(16, 'Password must not be greater than 16 characters')
+        .required('Please type your password'),
+
+});
+
+    export const RegistrationForm = () => (
+        <Formik initialValues={{name:'', email:'', password:''}}
+            validationSchema={FormValid}
+        onSubmit={(values, { setSubmitting }) => {
+            console.log('Form values: ', values)
+            setSubmitting(false)
+        }}>
+            {({isSubmitting}) => (
+                <Form className="flex flex-col gap-5 border-gray-500 border-4 rounded-xl p-4 w-[512px]">
+                    <h1 className='text-2xl font-bold'>Registration Form</h1>
+                    <div className="flex flex-col gap-2 m-2">
+                        <span className='text-left'>Name:</span>
+                    <Field className='border-2 border-gray-300 rounded-lg p-2' type="text" name="name" placeholder="Name" />
+                    <ErrorMessage className='text-red-600' name="name" component="div" />
+                    </div>
+
+                    <div className="flex flex-col gap-2 m-2">
+                        <span className='text-left'>Email:</span>
+                    <Field className='border-2 border-gray-300 rounded-lg p-2' type="email" name="email" placeholder="Email" />
+                    <ErrorMessage className='text-red-600' name="email" component="div" />
+                    </div>
+
+                    <div className="flex flex-col gap-2 m-2">
+                        <span className='text-left'>Password:</span>
+                    <Field className='border-2 border-gray-300 rounded-lg p-2' type="password" name="password" placeholder="Password" />
+                    <ErrorMessage className='text-red-600' name="password" component="div" />
+                    </div>
+
+                    <button type="submit" disabled={isSubmitting}>
+                        Submit
+                    </button>
+
+                </Form>
+            )}
+        </Formik>
+    )
