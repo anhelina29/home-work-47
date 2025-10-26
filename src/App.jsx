c1292e65f5ffb5122c827bef54fc56ef4c7a4469
anhelina29 <linakh358@gmail.com>
2025-10-26 18:50:37 +0200

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {RegistrationForm} from "./components/Form.jsx";
+import {RegistrationForm} from "./components/RegistrationForm";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,7 +1,7 @@
 import {Formik, Form, Field, ErrorMessage} from 'formik'
 import {object, string} from 'yup';
 
-export const FormValid = () => object().shape({
+const validationShema = object().shape({
     name: string()
         .min(2, 'Min length must be greater than 2 characters')
         .max(50, 'Max length must not be greater than 50 characters')
@@ -18,39 +18,44 @@ export const FormValid = () => object().shape({
 
 });
 
+const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+}
+
+const handleSubmit = (values: typeof initialValues) => {
+    console.log("Form values: ", values)
+}
+
     export const RegistrationForm = () => (
-        <Formik initialValues={{name:'', email:'', password:''}}
-            validationSchema={FormValid}
-        onSubmit={(values, { setSubmitting }) => {
-            console.log('Form values: ', values)
-            setSubmitting(false)
-        }}>
-            {({isSubmitting}) => (
+        <Formik initialValues={initialValues}
+            validationSchema={validationShema}
+        onSubmit={handleSubmit}>
                 <Form className="flex flex-col gap-5 border-gray-500 border-4 rounded-xl p-4 w-[512px]">
                     <h1 className='text-2xl font-bold'>Registration Form</h1>
                     <div className="flex flex-col gap-2 m-2">
-                        <span className='text-left'>Name:</span>
+                        <label htmlFor='name' className='text-left'>Name:</label>
                     <Field className='border-2 border-gray-300 rounded-lg p-2' type="text" name="name" placeholder="Name" />
                     <ErrorMessage className='text-red-600' name="name" component="div" />
                     </div>
 
                     <div className="flex flex-col gap-2 m-2">
-                        <span className='text-left'>Email:</span>
+                        <label htmlFor='email' className='text-left'>Email:</label>
                     <Field className='border-2 border-gray-300 rounded-lg p-2' type="email" name="email" placeholder="Email" />
                     <ErrorMessage className='text-red-600' name="email" component="div" />
                     </div>
 
                     <div className="flex flex-col gap-2 m-2">
-                        <span className='text-left'>Password:</span>
+                        <label htmlFor='password' className='text-left'>Password:</label>
                     <Field className='border-2 border-gray-300 rounded-lg p-2' type="password" name="password" placeholder="Password" />
                     <ErrorMessage className='text-red-600' name="password" component="div" />
                     </div>
 
-                    <button type="submit" disabled={isSubmitting}>
+                    <button type="submit">
                         Submit
                     </button>
 
                 </Form>
-            )}
         </Formik>
     )
